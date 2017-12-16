68ae235c720e3b274e88157403235087aa191c9b
Besim ALTINOK <besimaltnok@gmail.com>
2017-12-17 00:52:53 +0300

    Update pisavar.py

--- pisavar.py
+++ pisavar.py
@@ -1,56 +1,66 @@
 # coding=utf-8
+
+# Modulles
 import sys
+import time
 import argparse
-from scapy.layers.dot11 import Dot11, Dot11Deauth, RadioTap, Dot11Beacon
-from scapy.sendrecv import sendp
-from scapy.all import *
-from time import gmtime, strftime
 from termcolor import colored
+from argparse import RawTextHelpFormatter
+import commands
+import netifaces
+from scapy.all import *
+from termcolor import colored
+from scapy.sendrecv import sendp
+from time import gmtime, strftime
+from scapy.layers.dot11 import Dot11, Dot11Deauth, RadioTap, Dot11Beacon, Dot11Elt
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 
 banner_intro = """
------------------------------------------------------
-[---]    ______ _ _____                         [---]
-[---]    | ___ (_)  ___|                        [---]
-[---]    | |_/ /_\ `--.  __ ___   ____ _ _ __   [---]
-[---]    |  __/| |`--. \/ _` \ \ / / _` | '__|  [---]
-[---]    | |   | /\__/ / (_| |\ V / (_| | |     [---]
-[---]    \_|   |_\____/ \__,_| \_/ \__,_|_|     [---]
-[---]                                           [---]
-[---]     Just for fun and security @octosec    [---]
-[---]     W:besimaltinok.com | T:altnokbesim    [---]
-[---]               G:besimaltnok               [---]
------------------------------------------------------
 
+██████╗ ██╗███████╗ █████╗ ██╗   ██╗ █████╗ ██████╗
+██╔══██╗██║██╔════╝██╔══██╗██║   ██║██╔══██╗██╔══██╗
+██████╔╝██║███████╗███████║██║   ██║███████║██████╔╝
+██╔═══╝ ██║╚════██║██╔══██║╚██╗ ██╔╝██╔══██║██╔══██╗
+██║     ██║███████║██║  ██║ ╚████╔╝ ██║  ██║██║  ██║
+╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚═╝  ╚═╝╚═╝  ╚═╝
+
+----------------------------------------------------
 """
 
-banner_dos = """
-     ______      _____    ___  _   _             _
-     |  _  \    /  ___|  / _ \| | | |           | |
-     | | | |___ \ `--.  / /_\ \ |_| |_ __ _  ___| | __
-     | | | / _ \ `--. \ |  _  | __| __/ _` |/ __| |/ /
-     | |/ / (_) /\__/ / | | | | |_| || (_| | (__|   <
-     |___/ \___/\____/  \_| |_/\__|\__\__,_|\___|_|\_\
-
-
-         Sents deauthentication attack to TARGET(s)
-"""
 
 DESCRIPTION = """
-The goal of this project is to find out the fake access points opened by the WiFi pineapple device using 
-the PineAP module and to prevent clients from being affected by initiating a deauthentication attack to 
+About project:
+
+The goal of this project is to find out the fake access points opened by the WiFi pineapple device using
+the PineAP module and to prevent clients from being affected by initiating a deauthentication attack to
 the attacking device.
+
+
+Pisavar Methods = 
+
+1 : Identify and log only PineAP activities
+2 : Identify, attack and log PineAP activities
+--------------------------------------------------------------------
 """
 
-parser = argparse.ArgumentParser('PiSavar', description=DESCRIPTION)
-parser.add_argument('-i', '--interface', type=str, required=True, help="Interface")
+parser = argparse.ArgumentParser('PiSavar', description=DESCRIPTION, formatter_class=RawTextHelpFormatter)
+parser.add_argument('-pm','--pisavar-method', required=True, dest="attack_method", type=str, help="Pisavar attack methods")
+parser.add_argument('-i', '--interface',required=True, help="Interface (Monitor Mode)", type=str)
 args = parser.parse_args()
 
+
+def logging(log):
+	with open("/var/log/pisavar.log", "a") as f:
+		f.write(str(log)+"\n")
+		f.flush()
+		f.close()
 
 def sniff_channel_hop(iface):
     for i in range(1, 14):
         os.system("iwconfig " + iface + " channel " + str(i))
-        sniff(iface=iface, count=3, prn=air_scan)
+        sniff(iface=iface, count=4, prn=air_scan)
 
 
 def air_scan(pkt):
@@ -61,17 +71,17 @@ def air_scan(pkt):
     """
     if pkt.haslayer(Dot11Beacon):
         ssid, bssid = pkt.info, pkt.addr2
-        info = "{}=={}".format(bssid, ssid)
+        info = "{}=*={}".format(bssid, ssid)
         if info not in info_list:
             info_list.append(info)
 
 
-def pp_analysis(info_list, pp):
+def pp_analysis(info_list, pp, pisavar_method):
     """
     Analysis air_scan result for pineAP Suite detection
     """
     for i in info_list:
-        bssid, ssid = i.split("==")
+        bssid, ssid= i.split("=*=")
         if bssid not in pp.keys():
             pp[bssid] = []
             pp[bssid].append(ssid)
@@ -83,10 +93,18 @@ def pp_analysis(info_list, pp):
     """
     for v in pp.keys():
         if len(pp[v]) >= 2 and v not in blacklist:
+            print colored("\033[1m[*] PineAP module activity was detected.", 'magenta', attrs=['reverse', 'blink'])
+            print "\033[1m[*] MAC Address : ", v
+            print "\033[1m[*] FakeAP count: ", len(pp[v])
+ 	    log_time = time.strftime("%c")
             blacklist.append(v)
-            print "\033[1m[--] DETECTED PineAP ATTACK\n "
-            print "\033[1m[--] MAC Address: ", v
-            print "\033[1m[--] Fake Access Points :", len(pp[v])
+            if pisavar_method == "2":
+                pp_deauth(blacklist)
+                log = log_time, "||", v, " - ", len(pp[v]), " - Deauth Attack"
+                logging(log)
+            elif pisavar_method == "1":
+                log = log_time, "||", v, " - ", len(pp[v])
+                logging(log)
     time.sleep(3)
     return blacklist
 
@@ -102,54 +120,41 @@ def pp_deauth(blacklist):
     """
     Starts deauthentication attack for PineAP Suite.
     """
-    os.system("reset")
-    print "\033[1m" + banner_dos
-    print colored("\n-------------------  ̿' ̿'\̵͇̿̿\з=(◕_◕)=ε/̵͇̿̿/'̿'̿  ---------------------\n̿", "red")
-    print "\033[1m[--] TARGET(s): ", blacklist
-    time.sleep(1)
-    print "\033[1m[*] Attacking for affected devices"
+    attack_start = "[*] Attack has started for " + str(blacklist)
+    print colored(attack_start, 'red', attrs=['reverse', 'blink'])
     time.sleep(2)
-    print "\033[1m[*] Channel number being set\n"
-    time.sleep(2)
-    for d in blacklist:
-        clist = sniff(iface=iface, count=30)
-        channel = find_channel(clist, d)
-        os.system("iwconfig wlan0mon channel " + str(channel))
-        print "\033[1m[*] Sending 120 deauthentication packets to ", d
-        deauth = RadioTap() / Dot11(addr1="ff:ff:ff:ff:ff:ff", addr2=d.lower(), addr3=d.lower()) / Dot11Deauth()
+    channel = 1
+    for target in blacklist:
+        clist = sniff(iface=iface, count=50)
+        channel = find_channel(clist, target)
+        deauth = RadioTap() / Dot11(addr1="ff:ff:ff:ff:ff:ff", addr2=target.lower(), addr3=target.lower()) / Dot11Deauth()
         sendp(deauth, iface=iface, count=120, inter=.2, verbose=False)
         time.sleep(1)
-    print "\033[1m[*] Attack completed"
+    print colored("[*] Attack has completed..", 'green', attrs=['reverse', 'blink'])
     time.sleep(2)
 
 
 if __name__ == '__main__':
+    path = "/var/log/pisavar/pisavar.log"
     iface = args.interface
+    mode  = "Monitor"
+    pisavar_method = args.attack_method
+    os.system("reset")
+    now = time.strftime("%c")
+    print banner_intro
+    print "Information about test:"
+    print "----------"*5
+    print "[*] Start time: ", now
+    print "[*] Detects PineAP module activity and starts deauthentication attack \n    (for fake access points - WiFi Pineapple Activities Detection) "
+    print "------------"*7
     while True:
-        channel = 0
-        blacklist = []
-        info_list = []
-        pp = {}
-        os.system("reset")
-        sys.stdout.write("\x1b[8;{rows};{cols}t".format(rows=28, cols=70))
-        print "\033[1m" + banner_intro
-        print "\n\033[1m[--] Start time: ", strftime("%Y-%m-%d %H:%M:%S", gmtime())
-        os.system("ifconfig " + iface + " down && iwconfig " + iface + " mode Monitor && ifconfig " + iface + " up")
-        time.sleep(4)
-        print "\033[1m[--] Started sniffing"
-        time.sleep(4)
-        print colored("\033[1m[--] Packets are being analysis", "green")
-        sniff_channel_hop(iface)
-        print "\033[1m[--] Total network: ", len(info_list)
-        time.sleep(2)
-        print "\033[1m[--] Searching for PineAP Suite traces"
-        time.sleep(5)
-        print "\033[1m=====================================\n"
-        blacklist = pp_analysis(info_list, pp)
-        time.sleep(2)
-        if len(blacklist):
-            sys.stdout.write("\x1b[8;{rows};{cols}t".format(rows=19, cols=70))
-            pp_deauth(blacklist)
-        else:
-            print "\033[1m# -- > NO TRACE FOUND :)\n"
-            time.sleep(4)
+	    time.sleep(10)
+	        channel = 0
+	        blacklist = []
+	        info_list = []
+	        pp = {}
+	        sniff_channel_hop(iface)
+	        blacklist = pp_analysis(info_list, pp, pisavar_method)
+	        time.sleep(2)
+	        if len(blacklist)!=0:
+		    	print "--------"*5
